b3967eb4aed68466b29102f6246b5c18b2cf8d3e
pobertt <79865756+pobertt@users.noreply.github.com>
2024-06-01 16:41:28 +0100

    creating project/adding files

---
+++ Source/OGAMProject/OGAMProject.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "OGAMProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, OGAMProject, "OGAMProject" );
+ 
---
+++ Source/OGAMProject/OGAMProjectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OGAMProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AOGAMProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AOGAMProjectGameMode();
+};
+
+
+
