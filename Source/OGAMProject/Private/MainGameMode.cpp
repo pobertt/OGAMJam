115deacb96843f7c6238429ea3a7102cdc178bb1
pobertt <79865756+pobertt@users.noreply.github.com>
2024-06-01 20:34:11 +0100

    changing things

---
+++ Source/OGAMProject/Private/MainGameMode.cpp
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MainGameMode.h"
+#include "MainGameState.h"
+
+
+AMainGameMode::AMainGameMode()
+{
+	GameStateClass = AMainGameState::StaticClass();
+
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Player/BP_PlayerCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
+
+int32 AMainGameMode::GetTestCount() const
+{
+	return GetGameState<AMainGameState>()->TestCount;
+}
+
+
+void AMainGameMode::SetTestCount(int32 newTestCount)
+{
+	GetGameState<AMainGameState>()->TestCount = newTestCount;
+}
+
+
+void AMainGameMode::StartPlay()
+{
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("Start Play called"));
+
+	//StartPlayEvent();
+
+	/* Initialize stuff here */
+
+	GetWorld()->GetAuthGameMode()->GetGameState<AMainGameState>()->TestCount = 150;
+
+	Super::StartPlay();
+}
+/*
+void AMainGameMode::StartPlayEvent()
+{
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("StartPlayEvent Default Implementation called"));
+}
+*/
---
+++ Source/OGAMProject/Public/MainGameMode.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MainGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OGAMPROJECT_API AMainGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMainGameMode();
+	virtual ~AMainGameMode() = default;
+
+	/* Return the number of the test count in the game */
+	UFUNCTION(BlueprintPure, Category = "Test")
+		virtual int32 GetTestCount() const;
+
+	
+	UFUNCTION(BlueprintCallable, Category = "Test")
+		virtual void SetTestCount(int32 newTestCount);
+	
+	virtual void StartPlay();
+	/*
+	UFUNCTION(BlueprintNativeEvent, Category = "Test", DisplayName = "Start Play")
+		void StartPlayEvent();
+		*/
+};
